993c834e2ae39f4786cfe5246892e536cf830c4d
prana <prana@PranavComputer>
2020-10-06 19:46:42 -0700

    Step one Commit, added javadoc comments on program, no alteration to method blocks

---
+++ Test/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,75 @@
+package cse360assignment02;
+
+/**
+*
+* @author Pranav Annapareddi 
+* This class is our first assignment involving code
+* At version and since do not apply here. 
+* 
+*/
+
+
+public class AddingMachine {
+	
+	/**
+	 * Private integer total that will hold the value of end result
+	 */	
+	  private int total;
+	  
+	  /**
+	   * Constructor for this class
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  
+	  /**  
+	   * getter function
+	   * @return the value of total 
+	   */
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  
+	  /**
+	   * this method adds the inputted value to total
+	   * @param value-  the amount needed to calculation
+	   * @return the amount of health hero has after attack
+	   *
+	   */
+	  
+	  public void add (int value) {
+	  }
+	  
+	  
+	  /**
+	   * method for subtracting inputed value
+	   * @param value - the number used in this method
+	   * @return the amount of health hero has after attack
+	   */
+
+	  public void subtract (int value) {
+	  }
+	  
+	  
+	  /**
+	   * @return the string format of the value
+	   * Need to understand need of function
+	   */
+
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  
+	  /**
+	   * Clear value of add machine
+	   * 
+	   */
+
+	  public void clear() {
+	  }
+	}
